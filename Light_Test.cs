63ee2fba41a0453c866e3ee945228cdfbe64d4e7
Jmikey2011 <kellyjm3@miamioh.edu>
2015-04-14 12:59:59 -0400

    Final Update
    Hopefully the last update for this file.

--- Light_Test.cs
+++ Light_Test.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class Light_Test : MonoBehaviour {
 
-	static public float timeLimit = 10f;
+	static public float timeLimitYellow = 5f;
+	static public float timeLimitGreen = 10f;
+	static public float timeLimitRed = 15f;
+	
 	float currentTime;
-	public int light = 0;
+	
+	public int lightCounter = 0;
 	
 	public GameObject greenLight;
 	public GameObject yellowLight;
@@ -14,49 +18,79 @@ public class Light_Test : MonoBehaviour {
 	private float startTime;
 
 	// Use this for initialization
-	void Start () {
+	void Start (){
 		
 		startTime = Time.time;
+		SetLight ();
 		
 	}// end Start
 
 	// Update is called once per frame
 	// Sets the currentTime and calls SetLight function
-	void Update () {
+	void Update ()
+	{
 		
 		currentTime = Time.time;
+		switch (lightCounter) {
+		case 0:	
+			
+			if (currentTime - startTime > timeLimitRed) {
+				startTime = currentTime;
+				Debug.Log ("Time: " + startTime);
+				SetLight ();
+			}// end if 
+			return;	
+			
+		case 1:
+			
+			if (currentTime - startTime > timeLimitGreen) {
+				startTime = currentTime;
+				Debug.Log ("Time: " + startTime);
+				SetLight ();
+			}// end if 
+			return;
+
+		case 2:
+			
+			if (currentTime - startTime > timeLimitYellow) {
+				startTime = currentTime;
+				Debug.Log ("Time:  " + startTime);
+				SetLight ();
+			}// end if
+			return;
 		
-		if (currentTime - startTime > timeLimit) {
-			startTime = currentTime;
-			Debug.Log ("Time: " + startTime);
-			SetLight ();
-			light++;
-		}// end if
-		
+		}// end switch
 	}// end Update
 
 	// Turns on the specified light for the current frame
-	void SetLight () {	
+	void SetLight ()
+	{	
 		
 		greenLight.SetActive (false);
 		yellowLight.SetActive (false);
 		redLight.SetActive (false);
 		
-		if (light => 3 || light == 0) {
-			light = 0;
+		switch (lightCounter) {
+			
+		case 0:
+			lightCounter++;
 			Debug.Log ("Trying light 0");
 			greenLight.SetActive (true);
-		}// end if
-		
-		if (light == 1) {
+			return;
+	
+		case 1:
+			lightCounter++;
 			Debug.Log ("Trying light 1");
 			yellowLight.SetActive (true);
-		}// end if
+			return;
 		
-		if (light == 2) {
+		case 2: 
+			lightCounter = 0;
 			Debug.Log ("Trying light 2");
-			redLight.SetActive(true);
-		}// end if
+			redLight.SetActive (true);
+			return;
+			
+		}// end switch
 		
 	}// end  SetLight
 
